173ac3e2eb70cc6a5cbf1f3d646e91ab0edc42a0
julyanaflores <julyanafloresdp@gmail.com>
2021-11-03 22:45:53 -0300

    added material module imports and route to navigate to inital page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
 
 const routes: Routes = [
-    { path: 'login', component: LoginScreenComponent }
+    { path: 'login', component: LoginScreenComponent },
+    { path: 'initial-page', component: InitialPageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { ApplicationModule, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarModule, MatToolbarRow } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
@@ -13,19 +12,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // TODO: verificar pq o angular não está reconhecendo os componentes do Material Design
   declarations: [
     AppComponent,
-    LoginScreenComponent
+    LoginScreenComponent,
+    InitialPageComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
     FormsModule,
@@ -41,7 +44,7 @@ import { LoginScreenComponent } from './login-screen/login-screen.component';
     MatSelectModule,
     MatOptionModule,
     MatProgressSpinnerModule,
-    MatSlideToggleModule
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
